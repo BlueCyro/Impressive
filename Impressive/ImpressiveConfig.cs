14f7b4c1066e116cf513e50c07998b00367cc743
Cyro <BlueCyro@protonmail.com>
2023-12-05 00:02:03 -0600

    Add config option for enabled state

--- Impressive/ImpressiveConfig.cs
+++ Impressive/ImpressiveConfig.cs
@@ -6,5 +6,9 @@ namespace Impressive;
 public partial class Impressive : ResoniteMod
 {
     [AutoRegisterConfigKey]
+    internal static ModConfigurationKey<bool> Enabled_Config = new("Enabled", "When checked, enables listening on the specified OSC port");
+    public static bool Enabled => Config!.GetValue(Enabled_Config);
+    
+    [AutoRegisterConfigKey]
     internal static ModConfigurationKey<int> Port_Config = new("Port", "The port on which OSC will listen", () => 8000);
 }
