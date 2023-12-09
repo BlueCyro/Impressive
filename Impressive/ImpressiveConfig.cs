331660e5dc86d242f106c38ca95b2f4ba1d69f97
Cyro <BlueCyro@protonmail.com>
2023-12-09 02:16:20 -0600

    Add default value for enabled so we don't instantly kill people

--- Impressive/ImpressiveConfig.cs
+++ Impressive/ImpressiveConfig.cs
@@ -6,7 +6,7 @@ namespace Impressive;
 public partial class Impressive : ResoniteMod
 {
     [AutoRegisterConfigKey]
-    internal static ModConfigurationKey<bool> Enabled_Config = new("Enabled", "When checked, enables listening on the specified OSC port");
+    internal static ModConfigurationKey<bool> Enabled_Config = new("Enabled", "When checked, enables listening on the specified OSC port", () => true);
     public static bool Enabled => Config!.GetValue(Enabled_Config);
     
     [AutoRegisterConfigKey]
