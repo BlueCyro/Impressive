68a679bd020f7e2ca552f2383775e584bc8fd4e5
Cyro <BlueCyro@protonmail.com>
2023-12-08 20:12:30 -0600

    Rename steam face to steam eyes

---
+++ SteamLink/SteamEyes.cs
@@ -0,0 +1,124 @@
+using System.Runtime.InteropServices;
+using Elements.Core;
+using ReSounding;
+
+namespace Impressive;
+public class SteamEyes
+{
+    public SteamLinkEye EyeLeft = new();
+    public SteamLinkEye EyeRight = new();
+    public SteamLinkEye EyeCombined = new();
+
+
+    #region EyesDir
+
+    [OSCMap("/sl/eyeTrackedGazePoint")]
+    public float3 LookDir { set => EyeCombined.EyeDirection = value; }
+
+    // Center vec (whatever that means I guess)
+    [OSCMap("/tracking/eye/CenterVecFull")]
+    public float3 CenterVecFull;
+    
+
+    // Left eye direction
+    [OSCMap("/avatar/parameters/LeftEyeX")]
+    public float LeftEyeX { set => EyeLeft.SetDirectionFromXY(X: -value); }
+
+    [OSCMap("/avatar/parameters/LeftEyeY")]
+    public float LeftEyeY { set => EyeLeft.SetDirectionFromXY(Y: -value); }
+
+    
+
+    // Right eye direction
+    [OSCMap("/avatar/parameters/RightEyeX")]
+    public float RightEyeX { set => EyeRight.SetDirectionFromXY(X: -value); }
+
+    [OSCMap("/avatar/parameters/RightEyeY")]
+    public float RightEyeY { set => EyeRight.SetDirectionFromXY(Y: -value); }
+
+    
+
+    #endregion
+
+    #region Eyelids
+
+    // Right eyes
+    [OSCMap("/avatar/parameters/RightEyeLid")]
+    public float RightEyeLid { set => EyeRight.Eyelid = 1f - MathX.Pow(value, 1.0f / 3.0f); }
+
+    [OSCMap("/sl/xrfb/facew/InnerBrowRaiserR")] // This particular parameter seems to be both for eyebrows *and* for eye widening.
+    public float RightEyeLidExpandedSqueeze
+    {
+        set
+        {
+            EyeRight.ExpandedSqueeze = value;
+            EyeCombined.ExpandedSqueeze = MathX.Average(value, EyeLeft.ExpandedSqueeze);
+        }
+    }
+
+
+    [OSCMap("/avatar/parameters/RightEyeSqueezeToggle")]
+    public int RightEyeSqueezeToggle { set => EyeRight.SqueezeToggle = value; }
+
+    [OSCMap("/avatar/parameters/RightEyeWidenToggle")]
+    public int RightEyeWidenToggle { set => EyeRight.WidenToggle = value; }
+
+
+
+    // Left eyes
+    [OSCMap("/avatar/parameters/LeftEyeLid")]
+    public float LeftEyeLid { set => EyeLeft.Eyelid = 1f - MathX.Pow(value, 1.0f / 3.0f); }
+
+    [OSCMap("/sl/xrfb/facew/InnerBrowRaiserL")] // The left one is also for both eye widening and eyebrow raising.
+    public float LeftEyeLidExpandedSqueeze
+    {
+        set
+        {
+            EyeLeft.ExpandedSqueeze = value;
+            EyeCombined.ExpandedSqueeze = MathX.Average(value, EyeRight.ExpandedSqueeze);
+        }
+    }
+
+
+    [OSCMap("/avatar/parameters/LeftEyeSqueezeToggle")]
+    public int LeftEyeSqueezeToggle { set => EyeLeft.SqueezeToggle = value; }
+
+    [OSCMap("/avatar/parameters/LeftEyeWidenToggle")]
+    public int LeftEyeWidenToggle { set => EyeLeft.WidenToggle = value; }
+
+
+
+    [OSCMap("/tracking/eye/EyesClosedAmount")]
+    public float EyesClosedAmount { set => EyeCombined.Eyelid = value; }
+
+    #endregion
+}
+public struct SteamLinkEye
+{
+    private static readonly floatQ compensate = floatQ.Euler(new(0, -90, 0));
+
+    public float3 EyeDirection;
+
+    private float DirX;
+    private float DirY;
+
+    public float Eyelid;
+
+    public float ExpandedSqueeze;
+    public int WidenToggle;
+    public int SqueezeToggle;
+
+    public void SetDirectionFromXY(float? X = null, float? Y = null)
+    {
+        DirX = X ?? DirX;
+        DirY = Y ?? DirY;
+
+        // I don't actually know if this is right, but it looks basically perfect
+        float xAng = MathX.Asin(DirX);
+        float yAng = MathX.Asin(DirY);
+        float xCos = MathX.Cos(xAng);
+        float yCos = MathX.Cos(yAng);
+
+        EyeDirection = compensate * new float3(xCos * yCos, DirY, DirX * yCos);
+    }
+}
